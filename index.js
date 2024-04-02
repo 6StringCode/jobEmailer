0c90817501d54eebd5417a0c761d551a93029462
adam stoler <adamstoler@adams-mbp.mynetworksettings.com>
2024-04-02 17:13:36 -0400

    testing cron job

--- index.js
+++ index.js
@@ -12,16 +12,15 @@ const nytJobUrl = 'https://boards-api.greenhouse.io/v1/boards/thenewyorktimes/jo
 let previousState = null
 
 setInterval(async () => {
-  console.log('setInterval')
   try {
     const response = await fetch(nytJobUrl)
     const data = await response.json()
 
     if (JSON.stringify(data) !== JSON.stringify(previousState)) {
       sendEmail(data)
+    } else {
+      sendEmailWithout(data)
     }
-
-    sendEmail(data)
 
     previousState = data
 
@@ -29,7 +28,7 @@ setInterval(async () => {
     console.log('Failed to fetch Jobs', error)
   }
   // }, 6 * 60 * 60 * 1000) //6hrs
-}, 10000) //6hrs
+}, 1000) //6hrs
 
 
 function sendEmail(jobData) {
@@ -50,9 +49,41 @@ function sendEmail(jobData) {
   const mailOptions = {
     from: email,
     to: email,
-    subject: 'NYT Jobs Update',
+    subject: `${jobData.jobs.length} NYT Jobs Updated`,
     text: 'The data has been updated',
     html: `<h1>NYT Job Update</h1>${jobsHtml}`
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error Sending Email:', error)
+    } else {
+      console.log('Email sent:', info.response)
+    }
+  })
+}
+
+function sendEmailWithout(jobData) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: email,
+      pass
+    }
+  })
+
+  const jobsHtml = jobData.jobs.map(job => {
+    const date = new Date(job.updated_at).toLocaleString();
+    return `<a href="${job.absolute_url}"><b>${job.title}</b> - Updated at: ${date}</a><br/>`;
+  }).join('')
+
+
+  const mailOptions = {
+    from: email,
+    to: email,
+    subject: `${jobData.jobs.length} NYT Jobs: No Updates`,
+    text: 'The data hasnt been updated',
+    html: `<h1>There have been no updates</h1>${jobsHtml}`
   }
 
   transporter.sendMail(mailOptions, (error, info) => {
