1639da337c5c3a97e31023d6ecf79b9401aa288f
adam stoler <adamstoler@adams-mbp.mynetworksettings.com>
2024-04-03 09:27:03 -0400

    refactored sendEmail into /api/sendEmail for testing with Vercel function

---
+++ api/sendEmail.js
@@ -0,0 +1,83 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer');
+
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+const nytJobUrl = 'https://boards-api.greenhouse.io/v1/boards/thenewyorktimes/jobs';
+
+let previousState = null
+
+const getJobs = async () => {
+  try {
+    const response = await fetch(nytJobUrl)
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    console.log('Failed to fetch Jobs', error)
+  }
+}
+
+
+async function sendEmail() {
+  const jobData = await getJobs()
+  // console.log(jobData.meta.total)
+  // console.log("me first")
+
+
+  emailHandler(jobData)
+
+
+  function emailHandler(jobData) {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: email,
+        pass
+      }
+    })
+
+    const jobsHtml = jobData.jobs.map(job => {
+      const date = new Date(job.updated_at).toLocaleString();
+      return `<a href="${job.absolute_url}"><b>${job.title}</b> - Updated at: ${date}</a><br/>`;
+    }).join('')
+
+
+    console.log("inside emailHandler", JSON.stringify(previousState) === JSON.stringify(jobData))
+
+    let mailOptions = (JSON.stringify(jobData) !== JSON.stringify(previousState)) ? {
+      from: email,
+      to: email,
+      subject: `${jobData.jobs.length} NYT Jobs Updated`,
+      text: 'The data has been updated',
+      html: `<h1>NYT Job Update</h1>${jobsHtml}`
+    } : {
+      from: email,
+      to: email,
+      subject: `${jobData.jobs.length} NYT Jobs: No Updates`,
+      text: 'The data hasnt been updated',
+      html: `<h1>There have been no updates</h1>${jobsHtml}`
+    }
+
+    console.log("mailOptions:", mailOptions.subject)
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error Sending Email:', error)
+      } else {
+        console.log('Email sent:', info.response)
+      }
+    })
+
+    previousState = jobData
+    console.log("end of handler", JSON.stringify(previousState) === JSON.stringify(jobData))
+  }
+}
+
+(async () => {
+  await sendEmail();
+
+  // Check the value of previousState here...
+})();
+
+setTimeout(async () => await sendEmail(), 2000)
--- index.js
+++ index.js
@@ -1,98 +1,117 @@
 require('dotenv').config()
 const express = require('express')
 const nodemailer = require('nodemailer')
+const { default: sendEmail } = require('./api/sendEmail')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const email = process.env.EMAIL;
-const pass = process.env.EMAIL_PASS;
-const nytJobUrl = 'https://boards-api.greenhouse.io/v1/boards/thenewyorktimes/jobs'
-
-let previousState = null
-
-setInterval(async () => {
-  try {
-    const response = await fetch(nytJobUrl)
-    const data = await response.json()
-
-    if (JSON.stringify(data) !== JSON.stringify(previousState)) {
-      sendEmail(data)
-    } else {
-      sendEmailWithout(data)
-    }
-
-    previousState = data
-
-  } catch (error) {
-    console.log('Failed to fetch Jobs', error)
-  }
-  // }, 6 * 60 * 60 * 1000) //6hrs
-}, 1000) //6hrs
+// const email = process.env.EMAIL;
+// const pass = process.env.EMAIL_PASS;
+// const nytJobUrl = 'https://boards-api.greenhouse.io/v1/boards/thenewyorktimes/jobs'
 
 
-function sendEmail(jobData) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: email,
-      pass
-    }
-  })
+// sendEmail()
+// setInterval(async () => {
+//   try {
+//     const response = await fetch(nytJobUrl)
+//     const data = await response.json()
 
-  const jobsHtml = jobData.jobs.map(job => {
-    const date = new Date(job.updated_at).toLocaleString();
-    return `<a href="${job.absolute_url}"><b>${job.title}</b> - Updated at: ${date}</a><br/>`;
-  }).join('')
+//     if (JSON.stringify(data) !== JSON.stringify(previousState)) {
+//       sendEmail(data)
+//     } else {
+//       sendEmailWithout(data)
+//     }
 
+//     previousState = data
 
-  const mailOptions = {
-    from: email,
-    to: email,
-    subject: `${jobData.jobs.length} NYT Jobs Updated`,
-    text: 'The data has been updated',
-    html: `<h1>NYT Job Update</h1>${jobsHtml}`
-  }
+//   } catch (error) {
+//     console.log('Failed to fetch Jobs', error)
+//   }
+//   // }, 6 * 60 * 60 * 1000) //6hrs
+// }, 1000) //6hrs
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log('Error Sending Email:', error)
-    } else {
-      console.log('Email sent:', info.response)
-    }
-  })
-}
+// async function getJobs() {
+//   const response = await fetch(nytJobUrl)
+//   const data = await response.json()
+//   const jobs = data.jobs
+//   console.log(data.meta.total)
+// }
 
-function sendEmailWithout(jobData) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: email,
-      pass
-    }
-  })
+// if (JSON.stringify(data) !== JSON.stringify(previousState)) {
+//   sendEmail(data)
+// } else {
+//   sendEmailWithout(data)
+// }
 
-  const jobsHtml = jobData.jobs.map(job => {
-    const date = new Date(job.updated_at).toLocaleString();
-    return `<a href="${job.absolute_url}"><b>${job.title}</b> - Updated at: ${date}</a><br/>`;
-  }).join('')
+// previousState = data
 
 
-  const mailOptions = {
-    from: email,
-    to: email,
-    subject: `${jobData.jobs.length} NYT Jobs: No Updates`,
-    text: 'The data hasnt been updated',
-    html: `<h1>There have been no updates</h1>${jobsHtml}`
-  }
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log('Error Sending Email:', error)
-    } else {
-      console.log('Email sent:', info.response)
-    }
-  })
-}
+
+
+// function sendEmail(jobData) {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: email,
+//       pass
+//     }
+//   })
+
+//   const jobsHtml = jobData.jobs.map(job => {
+//     const date = new Date(job.updated_at).toLocaleString();
+//     return `<a href="${job.absolute_url}"><b>${job.title}</b> - Updated at: ${date}</a><br/>`;
+//   }).join('')
+
+
+//   const mailOptions = {
+//     from: email,
+//     to: email,
+//     subject: `${jobData.jobs.length} NYT Jobs Updated`,
+//     text: 'The data has been updated',
+//     html: `<h1>NYT Job Update</h1>${jobsHtml}`
+//   }
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log('Error Sending Email:', error)
+//     } else {
+//       console.log('Email sent:', info.response)
+//     }
+//   })
+// }
+
+// function sendEmailWithout(jobData) {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: email,
+//       pass
+//     }
+//   })
+
+//   const jobsHtml = jobData.jobs.map(job => {
+//     const date = new Date(job.updated_at).toLocaleString();
+//     return `<a href="${job.absolute_url}"><b>${job.title}</b> - Updated at: ${date}</a><br/>`;
+//   }).join('')
+
+
+//   const mailOptions = {
+//     from: email,
+//     to: email,
+//     subject: `${jobData.jobs.length} NYT Jobs: No Updates`,
+//     text: 'The data hasnt been updated',
+//     html: `<h1>There have been no updates</h1>${jobsHtml}`
+//   }
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log('Error Sending Email:', error)
+//     } else {
+//       console.log('Email sent:', info.response)
+//     }
+//   })
+// }
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`))
