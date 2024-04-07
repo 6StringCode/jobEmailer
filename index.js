3da7967aeae69c2d3c2c8a99087485b196a1c6fe
adam stoler <adamstoler@adams-mbp.mynetworksettings.com>
2024-04-07 14:53:03 -0400

    comment out

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-const sendEmail = require('./api/sendEmail');
+// const sendEmail = require('./api/sendEmail');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,13 +15,13 @@ app.listen(port, () => console.log(`App running on port ${port}`))
 // })
 
 //old stuff for testing
-app.get('/api/sendEmail', async (req, res) => {
-  // try {
-  await sendEmail(res);
-  // } catch (error) {
-  //   console.log("error:", error)
-  // res.status(500).send('An error occurred while fetching jobs', error);
-  // }
-});
+// app.get('/api/sendEmail', async (req, res) => {
+//   // try {
+//   await sendEmail(res);
+//   // } catch (error) {
+//   //   console.log("error:", error)
+//   // res.status(500).send('An error occurred while fetching jobs', error);
+//   // }
+// });
 
 // app.post('/send-email', sendEmail);
