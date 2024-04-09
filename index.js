33c715a7237cd95c37273772f0487c74fbd536fd
adam stoler <adamstoler@adams-mbp.mynetworksettings.com>
2024-04-09 10:32:41 -0400

    adding req worked, deleted get route in index

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ app.use(express.json())
 app.listen(port, () => console.log(`App running on port ${port}`))
 
 
-app.get('/', (req, res) => {
-  res.send('Job Opening Emailer')
-})
+// app.get('/', (req, res) => {
+//   res.send('Job Opening Emailer')
+// })
 
 //old stuff for testing
-app.get('/api/sendEmail', async (req, res) => {
-  // try {
-  await sendEmail(res);
-  // } catch (error) {
-  //   console.log("error:", error)
-  // res.status(500).send('An error occurred while fetching jobs', error);
-  // }
-});
+// app.get('/api/sendEmail', async (req, res) => {
+//   // try {
+//   await sendEmail(res);
+//   // } catch (error) {
+//   //   console.log("error:", error)
+//   // res.status(500).send('An error occurred while fetching jobs', error);
+//   // }
+// });
 
 // app.post('/send-email', sendEmail);
