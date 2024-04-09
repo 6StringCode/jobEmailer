53490561f20d0df56054f90eaa1c3e5f8ecc366e
adam stoler <adamstoler@adams-mbp.mynetworksettings.com>
2024-04-09 10:34:50 -0400

    added back bucket download and comparison

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -41,9 +41,9 @@ const transporterHandler = async (mailOptions, callback) => {
 async function sendEmail(req, res) {
   const jobData = await getJobs()
   // console.log(jobData)
-  // previousState = await downloadJobsFromGCS(bucketName, 'test.txt')
-  // console.log(await previousState.meta)
-  // console.log(JSON.stringify(jobData) === JSON.stringify(previousState))
+  previousState = await downloadJobsFromGCS(bucketName, 'test.txt')
+  console.log(await previousState.meta)
+  console.log(JSON.stringify(jobData) === JSON.stringify(previousState))
   //TODO compare jobData with google bucket
 
 
